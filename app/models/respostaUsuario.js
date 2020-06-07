01a43083f073288dee05852b40b9261a5b04d010
Otávio Barbosa <otavioromualdo@gmail.com>
2020-06-07 12:02:31 -0300

    Mostrar informações sobre a alternativa escolhida pelo usuário

--- app/models/respostaUsuario.js
+++ app/models/respostaUsuario.js
@@ -6,13 +6,14 @@ const respostaUsuario = () => {}
 
 
 respostaUsuario.getById = (request, response) => {
-    connection.query('SELECT qu.*, q.numero, q.pergunta, q.simulado_id, s.simulado FROM questao_has_usuario AS qu '
+    connection.query('SELECT qu.*, q.numero, q.pergunta, q.simulado_id, s.simulado, a.opcao, a.texto, a.correta FROM questao_has_usuario AS qu '
         + ' INNER JOIN questao AS q ON q.id = qu.questao_id '
         + ' INNER JOIN simulado AS s ON s.id = q.simulado_id '
+        + ' INNER JOIN alternativa AS a ON a.id = qu.usuario_alternativa_id '
         + ' WHERE qu.id = ?', [request.params.id], (error, result) => {
-        if(error){
-            console.log('Error: ' + error.message)
-        }
+            if(error){
+                console.log('Error: ' + error.message)
+            }
         response.statusCode = 200
         return response.json({data: result})
     })
@@ -24,19 +25,19 @@ respostaUsuario.insert = (request, response) => {
         response.statusCode = 200
         return response.json({data: 'Requisição inválida'})
     }
-
+    
     connection.query('INSERT INTO questao_has_usuario (questao_id, usuario_id, usuario_alternativa_id) VALUES (?)', 
-        [
-            [request.body.questao_id, request.body.usuario_id, request.body.alternativa_id]
-        ], (error, result) => {
+    [
+        [request.body.questao_id, request.body.usuario_id, request.body.alternativa_id]
+    ], (error, result) => {
         if(error){
             console.log('Error: ' + error.message)
         }
-
+        
         response.statusCode = 200
         return response.json({data: result.insertId})
     }) 
-
+    
 }
 
 respostaUsuario.readRespostasSimulado = (request, response) => {
@@ -45,12 +46,13 @@ respostaUsuario.readRespostasSimulado = (request, response) => {
         response.statusCode = 200
         return response.json({data: 'Requisição inválida'})
     }
-
-    connection.query('SELECT qu.*, q.numero, q.pergunta, q.simulado_id, s.simulado FROM questao_has_usuario AS qu '
-        + ' INNER JOIN questao AS q ON q.id = qu.questao_id '
-        + ' INNER JOIN simulado AS s ON s.id = q.simulado_id '
-        + ' WHERE q.simulado_id = ? AND qu.usuario_id = ?', 
-        [request.body.simulado_id, request.body.usuario_id], (error, result) => {
+    
+    connection.query('SELECT qu.*, q.numero, q.pergunta, q.simulado_id, s.simulado, a.opcao, a.texto, a.correta FROM questao_has_usuario AS qu '
+    + ' INNER JOIN questao AS q ON q.id = qu.questao_id '
+    + ' INNER JOIN simulado AS s ON s.id = q.simulado_id '
+    + ' INNER JOIN alternativa AS a ON a.id = qu.usuario_alternativa_id '
+    + ' WHERE q.simulado_id = ? AND qu.usuario_id = ?', 
+    [request.body.simulado_id, request.body.usuario_id], (error, result) => {
         if(error){
             console.log('Error: ' + error.message)
         }
@@ -58,7 +60,7 @@ respostaUsuario.readRespostasSimulado = (request, response) => {
         response.statusCode = 200
         return response.json({data: result})
     }) 
-
+    
 }
 
 respostaUsuario.readRespostasUsuario = (request, response) => {
@@ -67,11 +69,12 @@ respostaUsuario.readRespostasUsuario = (request, response) => {
         response.statusCode = 200
         return response.json({data: 'Requisição inválida'})
     }
-
-    connection.query('SELECT qu.*, q.numero, q.pergunta, q.simulado_id, s.simulado FROM questao_has_usuario AS qu '
-        + ' INNER JOIN questao AS q ON q.id = qu.questao_id '
-        + ' INNER JOIN simulado AS s ON s.id = q.simulado_id '
-        + ' WHERE qu.usuario_id = ?', 
+    
+    connection.query('SELECT qu.*, q.numero, q.pergunta, q.simulado_id, s.simulado, a.opcao, a.texto, a.correta FROM questao_has_usuario AS qu '
+    + ' INNER JOIN questao AS q ON q.id = qu.questao_id '
+    + ' INNER JOIN simulado AS s ON s.id = q.simulado_id '
+    + ' INNER JOIN alternativa AS a ON a.id = qu.usuario_alternativa_id '
+    + ' WHERE qu.usuario_id = ?', 
         [request.body.usuario_id], (error, result) => {
         if(error){
             console.log('Error: ' + error.message)
